8623b20ac3b829de9977d99f5e8cf56d10f93c18
thanhkali <thanhkali@kali.kalipro>
2022-05-08 14:24:07 -0400

    thanhtv

--- src/main/java/controllers/UserServlet.java
+++ src/main/java/controllers/UserServlet.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("view","/views/Users/index.jsp");
         request.getRequestDispatcher("/views/layout.jsp").forward(request,response);
     }
 
